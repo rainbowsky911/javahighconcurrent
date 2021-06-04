c11c2088ae6a308b576137f7d9b5b99cb242e0f9
rainbowsky911 <zdwbmw@163.com>
2021-06-04 17:13:35 +0800

    update:错误的加锁

--- src/main/java/javahighconcurrent/ch2/BadLockOnInteger.java
+++ src/main/java/javahighconcurrent/ch2/BadLockOnInteger.java
@@ -11,9 +11,15 @@ public class BadLockOnInteger implements Runnable {
 
     @Override
     public void run() {
-        for (int k = 0; k < 10000; k++) {
-            synchronized (instance) {
+        for (int k = 0; k < 100000; k++) {
+            synchronized (instance) {//加锁成功
                 i++;
+           // synchronized (i) {
+                /**
+                 * 加锁失败。把锁加载变量i上有问题，因为i++的在真是执行时变成了Integer.valueOf(i.intValue()+1)
+                 * valueOf会倾向返回一个代表指定数值的Integer对象实例，因此i++的本质就是创建一个新的Integer对象，并将他的引用赋值给i
+                 *  在多线程见，并不一定能够看到同一个i对象，因为i对象一直在变，因此两个线程每次加锁可能都加在了不同的对象实例上。
+                 */
             }
         }
     }
