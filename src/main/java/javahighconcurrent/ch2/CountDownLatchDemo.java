1b32530b67dd577ad38181a6b1c16c2ac4051921
rainbowsky911 <zdwbmw@163.com>
2021-06-04 18:04:41 +0800

    update:语义注释

--- src/main/java/javahighconcurrent/ch2/CountDownLatchDemo.java
+++ src/main/java/javahighconcurrent/ch2/CountDownLatchDemo.java
@@ -5,6 +5,10 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * countDownLacth是一种多线程控制工具,简单成为倒计计数器。它可以让某一个线程等待直到倒计计数结束，再开始执行。
+ * 构造函数接收一个整数作为参数，即当前这个计数器的计数个数。
+ */
 public class CountDownLatchDemo implements Runnable {
 
     static final CountDownLatch end = new CountDownLatch(10);
@@ -13,6 +17,7 @@ public class CountDownLatchDemo implements Runnable {
     @Override
     public void run() {
         try {
+            //检查模拟任务
             Thread.sleep(new Random().nextInt(10) * 1000);
             System.out.println("check complate");
             end.countDown();
