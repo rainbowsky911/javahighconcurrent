a33120b629a4029d2a91aae53a669cb8f9d080a3
rainbowsky911 <zdwbmw@163.com>
2021-06-05 16:09:33 +0800

    add:RareLimit限流

---
+++ src/main/java/javahighconcurrent/ch3/RareLimitDemo.java
@@ -0,0 +1,56 @@
+package javahighconcurrent.ch3;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: zdw
+ * @Date: 2021/06/05/15:34
+ * @Description: RareLimit限流
+ */
+public class RareLimitDemo {
+
+    //每秒至多输出两条记录
+    static RateLimiter limiter = RateLimiter.create(2);
+
+
+    public static class Task implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(LocalDateTime.now());
+        }
+    }
+
+    public static void main(String[] args) {
+            test2();
+
+    }
+
+
+    public static void test() {
+        for (int i = 0; i < 10; i++) {
+            limiter.acquire();
+            new Thread(new Task()).start();
+        }
+    }
+
+
+    /**
+     * 当请求成功时，tryAcquire方法返回true。否则返回false，如果访问数据量超出限制。那么超出的部分直接丢弃，不在进行处理。
+     */
+    public static void test2(){
+        for (int i = 0; i < 100; i++) {
+            if (!limiter.tryAcquire()) {
+                continue;
+            }
+            new Thread(new Task()).start();
+        }
+    }
+
+
+
+}
