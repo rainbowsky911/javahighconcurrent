8c7d487a16f0af356c0bd151fba7f1a7b47ba015
rainbowsky911 <zdwbmw@163.com>
2021-06-07 11:02:02 +0800

    update:并行计算

--- src/main/java/javahighconcurrent/ch5/pipeline/Msg.java
+++ src/main/java/javahighconcurrent/ch5/pipeline/Msg.java
@@ -1,5 +1,9 @@
 package javahighconcurrent.ch5.pipeline;
 
+/**
+ * 计算(B+C)*B/2的结果
+ * 线程间携带结果进行信息交换订单载体。
+ */
 public class Msg {
     public double i;
     public double j;
--- src/main/java/javahighconcurrent/ch5/pipeline/Plus.java
+++ src/main/java/javahighconcurrent/ch5/pipeline/Plus.java
@@ -3,6 +3,10 @@ package javahighconcurrent.ch5.pipeline;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
+/**
+ * P1取得了封装两个操作数的Msg,并进行求和。将结果传递给乘法线程P2。当没有数据需要处理
+ * P1进行等待
+ */
 public class Plus implements Runnable {
 
     public static BlockingQueue<Msg> bq = new LinkedBlockingDeque<>();
